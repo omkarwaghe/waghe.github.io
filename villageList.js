90afaa36f54a7f25cd516be03564b1c1a9f04bd1
Hemangini Patel <hemanginiP@hexaware.com>
2019-04-26 17:18:16 +0530

    master

--- villageList.js
+++ villageList.js
@@ -1,6 +1,11 @@
-var myStringArray = ["Hello","World","omkar"];
-var arrayLength = myStringArray.length;
-for (var i = 0; i < arrayLength; i++) {
-    console.log(myStringArray[i]);
-    //Do something
-}
+var villageLoop = document.getElementById("loopForVillages");
+
+
+
+// var myStringArray = ["Hello","World","omkar"];
+// var arrayLength = myStringArray.length;
+// for (var i = 0; i < arrayLength; i++) {
+//     console.log(myStringArray[i]);
+//     //Do something
+    
+// }
